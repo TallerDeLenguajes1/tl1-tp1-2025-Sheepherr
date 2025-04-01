d034282da50915520972fa94f7b90280f4983139
Luciano <ovejeromarcosluciano@gmail.com>
2025-04-01 09:30:36 -0300

    Ejercicio 3 del punto 2 completo

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,16 @@
 
 int main () {
 
-    printf("Hola Mundo");
+    printf("Hola Mundo\n");
+
+    int num = 53, *punt, tama;
+
+    punt = &num;
+    tama = sizeof(num);
+
+
+    printf("Contenido del puntero: %d\nDireccion de memoria almacenada en el puntero: %p\nDireccion de memoria de la variable: %p\nDirecion de memoria del puntero: %p\nTamaño de memoria utilizado por la variable: %d", *punt, punt, &num, &punt, tama);
+
 
     return 0;
 }
