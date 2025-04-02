fea61f96e74de4491a24b61b9a38052d243ced3b
Luciano <ovejeromarcosluciano@gmail.com>
2025-04-02 19:35:16 -0300

    Ejercicio 2.4 terminado

--- tp1_2.c
+++ tp1_2.c
@@ -2,24 +2,34 @@
 
 void devolverCuadrado (int *num);
 void invertir(int *a,int *b);
-
+void ordenar (int *a, int *b);
 int main () {
 
-    int num, *num2,a,b, *p_a, *p_b;
+    int num, *num2, num3, num4, *p_n3, *p_n4, a, b, *p_a, *p_b;
     num2 = &num;
     p_a = &a;
     p_b = &b;
+    p_n3 = &num3;
+    p_n4 = &num4;
     printf("Ingrese el numero que desea elevar al cuadrado: ");
     scanf("%d", &num);
 
     devolverCuadrado(num2);
     
-    printf("Ingrese dos numeros para invertir entre ellos.\n num1: ");
+    printf("Ingrese dos numeros para invertir entre ellos.\nnum1: ");
     scanf("%d", &a);
     printf("num2: ");
     scanf("%d", &b);
     
     invertir(p_a, p_b);
+    
+    printf("Ingrese dos numeros para ordenar de mayor a menor.\nnum1: ");
+    scanf("%d", &num3);
+    printf("num2: ");
+    scanf("%d", &num4);
+
+    ordenar(p_n3,p_n4);
+
     return 0;
 }
 
@@ -37,6 +47,29 @@ void invertir (int *a, int *b) {
     *b = *a;
     *a = num;
 
-    printf("El nuevo valor de la variable a es: %d\nEl nuevo valor de la variable b es: %d", *a, *b);
+    printf("El nuevo valor de la variable a es: %d\nEl nuevo valor de la variable b es: %d\n", *a, *b);
 
+}
+void ordenar (int *a, int *b) {
+    int num;
+    if (*a > *b)
+    {
+
+        num = *b;
+        *b = *a;
+        *a = num;
+        printf("El numero mas grande es: %d\nEl nuemero mas chico es: %d", *b, *a);
+
+    } else if (*a = *b)
+    {
+
+        printf("Los numeros son iguales.");
+
+    } else
+    {
+
+        printf("El numero mas grande es: %d\nEl numero mas chico es: %d", *b, *a);
+
+    }
+    
 }
